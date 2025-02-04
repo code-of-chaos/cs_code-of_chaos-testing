8b1654f1426d3c3a80d028fafa947dbdb670441b
Anna Sas <anna.sas.dev@gmail.com>
2025-02-04 16:58:18 +0100

    Feat: Added missing extensions

--- src/CodeOfChaos.Testing.TUnit/Extensions/TUnitExtensionsDiagnosticsImmutableArray.cs
+++ src/CodeOfChaos.Testing.TUnit/Extensions/TUnitExtensionsDiagnosticsImmutableArray.cs
@@ -13,12 +13,33 @@ namespace CodeOfChaos.Testing.TUnit;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once InconsistentNaming
 public static class TUnitExtensionsDiagnosticsImmutableArray {
     public static InvokableValueAssertionBuilder<ImmutableArray<Diagnostic>> ContainsDiagnostic(this IValueSource<ImmutableArray<Diagnostic>> valueSource, string expectedId, [CallerArgumentExpression(nameof(expectedId))] string doNotPopulateThisValue1 = "") {
         return valueSource.RegisterAssertion(
             assertCondition: new ContainsDiagnosticAssertCondition<ImmutableArray<Diagnostic>>(
                 static diagnostics => diagnostics,
                 expectedId
+            ),
+            argumentExpressions: [doNotPopulateThisValue1]
+        );
+    }
+    
+    public static InvokableValueAssertionBuilder<ImmutableArray<Diagnostic>> DoesNotContainDiagnostic(this IValueSource<ImmutableArray<Diagnostic>> valueSource, string expectedId, [CallerArgumentExpression(nameof(expectedId))] string doNotPopulateThisValue1 = "") {
+        return valueSource.RegisterAssertion(
+            assertCondition: new DoesNotContainDiagnosticAssertCondition<ImmutableArray<Diagnostic>>(
+                static diagnostics => diagnostics,
+                expectedId
+            ),
+            argumentExpressions: [doNotPopulateThisValue1]
+        );
+    }
+
+    public static InvokableValueAssertionBuilder<ImmutableArray<Diagnostic>> ContainsDiagnosticsExclusively(this IValueSource<ImmutableArray<Diagnostic>> valueSource, string[] expectedIds, [CallerArgumentExpression(nameof(expectedIds))] string doNotPopulateThisValue1 = "") {
+        return valueSource.RegisterAssertion(
+            assertCondition: new ContainsDiagnosticsExclusivelyAssertCondition<ImmutableArray<Diagnostic>>(
+                static diagnostics => diagnostics,
+                expectedIds
             ),
             argumentExpressions: [doNotPopulateThisValue1]
         );
